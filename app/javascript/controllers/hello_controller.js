b73daed3a135cbee08682641bb649305e1a6c0c3
multikasia <kasia.ekiert@gmail.com>
2023-06-14 10:38:49 +0300

    working popover

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,10 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-import CharacterCounter from 'stimulus-character-counter'
-
 export default class extends Controller {
   connect() {
-    Stimulus.register('character-counter', CharacterCounter)
     this.element.textContent = "Hello World!"
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,9 +2,4 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 import { Application } from '@hotwired/stimulus'
-import Popover from 'stimulus-popover'
-
-application.register('popover', Popover)
 eagerLoadControllersFrom("controllers", application)
-
-const application = Application.start()
