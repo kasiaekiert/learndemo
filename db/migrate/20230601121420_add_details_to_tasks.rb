e098a111a089f7e376643cebb4fb63e369235bbd
multikasia <kasia.ekiert@gmail.com>
2023-06-15 11:36:15 +0200

    Add Rubocop and Rspec tests for model

--- db/migrate/20230601121420_add_details_to_tasks.rb
+++ db/migrate/20230601121420_add_details_to_tasks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDetailsToTasks < ActiveRecord::Migration[7.0]
   def change
     add_column :tasks, :deadline, :date
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  it 'is valid with name, content and deadline' do
+    task = Task.new(
+      name: 'Test task',
+      content: 'Test content',
+      deadline: Date.today + 1.day
+    )
+    expect(task).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    task = Task.new(name: nil)
+    task.valid?
+    expect(task.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a content' do
+    task = Task.new(content: nil)
+    task.valid?
+    expect(task.errors[:content]).to include("can't be blank")
+  end
+
+  it 'is invalid with a deadline in the past' do
+    task = Task.new(deadline: Date.today - 1.day)
+    task.valid?
+    expect(task.errors[:deadline]).to include("can't be in the past")
+  end
+end
