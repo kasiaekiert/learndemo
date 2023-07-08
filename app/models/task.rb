ee9108bedbba9cb4eba51fcbda765ce1249d50f6
kasiaekiert <45494643+kasiaekiert@users.noreply.github.com>
2023-07-08 15:01:29 +0200

    Add Rubocop and Rspec tests for model (#2)
    * Add Rubocop and Rspec tests for model
    * tests for validations with shoulda-matchers

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
   validates_presence_of :name, :content
   validate :deadline_cannot_be_in_the_past
- 
+
   def deadline_cannot_be_in_the_past
-    if deadline.present? && deadline < Date.today
-      errors.add(:deadline, "can't be in the past")
-    end
+    return unless deadline.present? && deadline < Date.today
+
+    errors.add(:deadline, "can't be in the past")
   end
 end
---
+++ spec/factories/tasks.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :task do
+    name { 'Task task' }
+    content { 'Test content' }
+    deadline { Date.current + 1.day }
+  end
+end
