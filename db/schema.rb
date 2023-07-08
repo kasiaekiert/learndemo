4ff0fa4a76d55327fe784f72fd9fb5e32663c6da
multikasia <kasia.ekiert@gmail.com>
2023-07-08 19:58:35 +0200

    Task is done by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_131010) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_175024) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "deadline"
-    t.boolean "done"
+    t.boolean "done", default: false
   end
 
 end
