52e47212526335a423f5bb5690afc65eb7432764
kasiaekiert <45494643+kasiaekiert@users.noreply.github.com>
2023-07-24 10:09:30 +0200

    Devise (#5)
    * devise views and user
    
    * Add navbar
    
    * resolved problem with sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get "/users/sign_out", as: "sign_out", to: "devise/sessions#destroy"
+  end
+  
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
