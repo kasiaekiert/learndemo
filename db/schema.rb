0acb8abaa86b5227f4c07eb0cbb4fa7519b268cf
multikasia <kasia.ekiert@gmail.com>
2023-08-11 20:34:14 +0200

    Add tasks to users

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_103956) do
     t.datetime "updated_at", null: false
     t.date "deadline"
     t.boolean "done", default: false
-    t.integer "user_id"
+    t.bigint "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +33,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_103956) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tasks", "users"
 end
