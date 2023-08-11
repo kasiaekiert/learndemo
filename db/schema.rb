9ec6ea883b61f64bd5913cb0aecf5ef284902326
multikasia <kasia.ekiert@gmail.com>
2023-08-11 13:39:01 +0200

    migration and add to model

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_140337) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_103956) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -20,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_140337) do
     t.datetime "updated_at", null: false
     t.date "deadline"
     t.boolean "done", default: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_140337) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tasks", "users"
 end
