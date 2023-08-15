0343b5fca59afa35963cfe79fb328bad4e1767ad
multikasia <kasia.ekiert@gmail.com>
2023-08-15 18:00:10 +0200

    gem annotate

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'rubocop', require: false
 gem 'rubocop-rspec', require: false
 gem 'rubocop-rails', require: false
 gem "pagy"
+gem 'annotate'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.5'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  deadline   :date
+#  done       :boolean          default(FALSE)
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
 class Task < ApplicationRecord
   validates_presence_of :name, :content
   validate :deadline_cannot_be_in_the_past
