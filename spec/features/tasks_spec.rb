83443382b9355e1e7b5a2d262933f0d5c7159bfd
multikasia <kasia.ekiert@gmail.com>
2023-09-16 16:00:25 +0200

    tests for 2 users with can can

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Tasks' do
   end
 
   describe 'User may add task which are visible only for him' do
-    it 'Add task as logged user' do
+    it 'Can do all CRUD actions with task as logged user' do
       visit root_path
       click_link 'New task'
       expect(page).to have_selector('.modal', text: 'New item')
@@ -23,18 +23,18 @@ RSpec.describe 'Tasks' do
       click_button 'Save'
       expect(page).to have_content 'Task was successfully created.'
       expect(page).to have_content 'Task content for user 1'
-    # end
 
-    # it 'Delete and update task as logged user' do
-      visit('/')
+    #delete and update task as logged user 
+      visit root_path
       click_link 'Show'
       click_link 'Edit this task'
       fill_in 'Name', with: 'Task title 123'
       click_button 'Save'
-
       expect(page).to have_content 'Task was successfully updated.'
 
       visit root_path
+      expect(page).to have_content('Tasks')
+
       click_link 'Edit'
       fill_in 'Content', with: 'Writing content 1233'
       click_button 'Save'
@@ -44,7 +44,7 @@ RSpec.describe 'Tasks' do
       click_link 'Delete'
       expect(page).to have_content 'Task was successfully destroyed.'
     end
-    
+
     context 'displays tasks for the specific user' do
       before do
         login_as user1
@@ -54,11 +54,26 @@ RSpec.describe 'Tasks' do
         visit root_path 
         expect(page).not_to have_content 'Task content for user 1'
 
+        click_link 'New task'
+        expect(page).to have_selector('.modal', text: 'New item')
+
+        fill_in 'Name', with: 'Task for user1'
+        fill_in 'Content', with: 'content for user 1'
+        click_button 'Save'
+        expect(page).to have_content 'Task was successfully created.'
+        expect(page).to have_content 'content for user 1'
+
+        logout 
+        login_as user1
+
+        visit root_path
+        expect(page).to have_content 'Task for user1'
+
         logout 
         login_as user
 
         visit root_path
-        expect(page).not_to have_content 'Task content for user 1'
+        expect(page).not_to have_content 'Task for user1'
       end
     end
   end
