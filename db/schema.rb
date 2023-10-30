222b74311255fec42fb192c6a94b73e834e9ac93
multikasia <kasia.ekiert@gmail.com>
2023-10-30 17:35:46 +0100

    model and table friendship created

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_24_184229) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_114024) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "task_categories", force: :cascade do |t|
@@ -49,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_184229) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "task_categories", "categories"
   add_foreign_key "task_categories", "tasks"
 end
